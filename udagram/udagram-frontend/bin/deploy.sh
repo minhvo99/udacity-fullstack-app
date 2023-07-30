10ce50a4b2a7154c20eca729ff6c04f9c78e601c
Minh Vo <minhvo2607dtu@gmail.com>
2023-07-30 15:39:22 +0700

    first commit

---
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -0,0 +1,2 @@
+aws s3 cp --recursive  ./www s3://udacity-bucket-287873351446
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udacity-bucket-287873351446
